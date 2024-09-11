ced45293ec23ad6e9d230a54cb4a6c2f59836f05
Fontan <fontanvillacampa@gmail.com>
2024-09-11 17:44:48 +1000

    parse thresholds

--- glomap_mapper.sh
+++ glomap_mapper.sh
@@ -16,6 +16,19 @@ BundleAdjustment_thres_loss_function=$(yq '.BundleAdjustment.thres_loss_function
 RelPoseEstimation_max_epipolar_error=$(yq '.RelPoseEstimation.max_epipolar_error // 1.0' $settings_yaml)
 GlobalPositioning_thres_loss_function=$(yq '.GlobalPositioning.thres_loss_function // 0.10000000000000001' $settings_yaml)
 
+Thresholds_max_epipolar_error_E=$(yq '.Thresholds.max_epipolar_error_E // 1.0' $settings_yaml)
+Thresholds_max_epipolar_error_F=$(yq '.Thresholds.max_epipolar_error_F // 4.0' $settings_yaml)
+Thresholds_max_epipolar_error_H=$(yq '.Thresholds.max_epipolar_error_H // 4.0' $settings_yaml)
+
+Thresholds_min_inlier_num=$(yq '.Thresholds.min_inlier_num // 30.0' $settings_yaml)
+Thresholds_min_inlier_ratio=$(yq '.Thresholds.min_inlier_ratio // 0.25' $settings_yaml)
+Thresholds_max_rotation_error=$(yq '.Thresholds.max_rotation_error // 10.0' $settings_yaml)
+
+Thresholds_max_angle_error=$(yq '.Thresholds.max_angle_error // 1.0' $settings_yaml)
+Thresholds_max_reprojection_error=$(yq '.Thresholds.max_reprojection_error // 0.01' $settings_yaml)
+Thresholds_min_triangulation_angle=$(yq '.Thresholds.min_triangulation_angle // 1.0' $settings_yaml)
+
+
 echo "    glomap mapper ..."
 database="${exp_folder_colmap}/colmap_database.db"
 
@@ -27,7 +40,16 @@ pixi run -e colmap glomap mapper \
     --RelPoseEstimation.max_epipolar_error "${RelPoseEstimation_max_epipolar_error}" \
     --GlobalPositioning.thres_loss_function "${GlobalPositioning_thres_loss_function}" \
     --BundleAdjustment.optimize_intrinsics 0 \
-    --BundleAdjustment.thres_loss_function "${BundleAdjustment_thres_loss_function}"
+    --BundleAdjustment.thres_loss_function "${BundleAdjustment_thres_loss_function}" \
+    --Thresholds.max_epipolar_error_E "${Thresholds_max_epipolar_error_E}" \
+    --Thresholds.max_epipolar_error_F "${Thresholds_max_epipolar_error_F}" \
+    --Thresholds.max_epipolar_error_H "${Thresholds_max_epipolar_error_H}" \
+    --Thresholds.min_inlier_num "${Thresholds_min_inlier_num}" \
+    --Thresholds.min_inlier_ratio "${Thresholds_min_inlier_ratio}" \
+    --Thresholds.max_rotation_error "${Thresholds_max_rotation_error}" \
+    --Thresholds.max_angle_error "${Thresholds_max_angle_error}" \
+    --Thresholds.max_reprojection_error "${Thresholds_max_reprojection_error}" \
+    --Thresholds.min_triangulation_angle "${Thresholds_min_triangulation_angle}"
 
 if [ "$verbose" -eq 1 ]; then
   pixi run -e colmap colmap gui --import_path "${exp_folder_colmap}/0" --database_path ${database} --image_path ${rgb_path}
