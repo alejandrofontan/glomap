cddc051dbac763df65dd3072245d7c74c41b420d
Fontan <fontanvillacampa@gmail.com>
2024-11-07 13:53:17 +1000

    collect colmap stats

--- glomap_reconstruction.sh
+++ glomap_reconstruction.sh
@@ -49,6 +49,23 @@ pixi run -e colmap ./Baselines/glomap/glomap_mapper.sh $sequence_path $exp_folde
 # Convert COLMAP outputs to a format suitable for VSLAM-Lab
 python Baselines/glomap/colmap_to_vslamlab.py $sequence_path $exp_folder $exp_id $verbose $rgb_txt
 
+# Get colmap stats
+colmap_stats_csv="${exp_folder}/${exp_id}_colmap_stats.csv"
+if [[ ! -f "$colmap_stats_csv" ]]; then
+  echo "File,Cameras,Images,Registered Images,Points,Observations,Mean Track Length,Mean Observations per Image,Mean Reprojection Error" > "$colmap_stats_csv"
+fi
+colmap_stats=$(pixi run -e colmap colmap model_analyzer --path "$exp_folder_colmap" 2>&1)
+cameras=$(echo "$colmap_stats" | grep -oP "(?<=Cameras: )\d+")
+images=$(echo "$colmap_stats" | grep -oP "(?<=Images: )\d+")
+registered_images=$(echo "$colmap_stats" | grep -oP "(?<=Registered images: )\d+")
+points=$(echo "$colmap_stats" | grep -oP "(?<=Points: )\d+")
+observations=$(echo "$colmap_stats" | grep -oP "(?<=Observations: )\d+")
+mean_track_length=$(echo "$colmap_stats" | grep -oP "(?<=Mean track length: )\d+\.\d+")
+mean_observations_per_image=$(echo "$colmap_stats" | grep -oP "(?<=Mean observations per image: )\d+\.\d+")
+mean_reprojection_error=$(echo "$colmap_stats" | grep -oP "(?<=Mean reprojection error: )\d+\.\d+")
+echo "$exp_folder_colmap,${cameras:-0},${images:-0},${registered_images:-0},${points:-0},${observations:-0},${mean_track_length:-0},${mean_observations_per_image:-0},${mean_reprojection_error:-0}" >> "$colmap_stats_csv"
+
+# Remove colmap data
 rm -rf ${exp_folder_colmap}
 
 
